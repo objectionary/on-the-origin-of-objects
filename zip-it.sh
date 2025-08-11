cc98b35b8b5b096d8c8f53f4dd1caaa5c7531db0
Yegor Bugayenko <yegor256@gmail.com>
2025-08-11 18:40:40 +0300

    Remove unnecessary files before zipping the project.

--- zip-it.sh
+++ zip-it.sh
@@ -28,6 +28,8 @@ pdflatex -interaction=errorstopmode -halt-on-error paper.tex > /dev/null
 pdflatex -interaction=errorstopmode -halt-on-error paper.tex > /dev/null
 rm -rf ./*.aux ./*.bcf ./*.blg ./*.fdb_latexmk ./*.fls ./*.log ./*.run.xml ./*.out ./*.exc
 rm -rf ./bibliography
+rm -rf ./iexec.ret
+rm -rf ./_eolang
 zip -x paper.pdf -r "paper-${version}.zip" ./*
 mv "paper-${version}.zip" ..
 cd ..
